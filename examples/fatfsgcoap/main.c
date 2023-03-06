d7c700e9c574904a89dac38d4e87c2708527f902
Jiawei-Lu <493460261@qq.com>
2023-03-06 00:49:51 +0000

    FATFSgcoap with working netif

---
+++ examples/fatfsgcoap/main.c
@@ -0,0 +1,507 @@
+/*
+ * Copyright (C) 2018 OTA keys S.A.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       File system usage example application
+ *
+ * @author      Vincent Dupont <vincent@otakeys.com>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "shell.h"
+#include "msg.h"
+
+/*GCoAP*/
+#include "net/gcoap.h"
+#include "shell.h"
+#include "gcoap_example.h"
+
+/*Kernel*/
+#include "kernel_defines.h"
+
+/*RPL*/
+#include "net/gnrc/rpl.h"
+#include "net/gnrc/rpl/structs.h"
+#include "net/gnrc/rpl/dodag.h"
+
+/*RTC*/
+#include "periph_conf.h"
+#include "periph/rtc.h"
+// #include "periph/rtc_mem.h"
+
+/*PM layer*/
+#include "periph/pm.h"
+#ifdef MODULE_PM_LAYERED
+#include "pm_layered.h"
+#endif
+
+/*IO1 Xplianed Extension Board*/
+#include "at30tse75x.h"
+#include "io1_xplained.h"
+#include "io1_xplained_params.h"
+#include "fmt.h"
+#include "periph/gpio.h"
+#include "board.h"
+
+/*Timer*/
+#include "timex.h"
+#include "ztimer.h"
+#include "xtimer.h"
+
+/*FAT Filesystem and VFS tool*/
+#include "fs/fatfs.h"
+#include "vfs.h"
+#include "mtd.h"
+// #include "vfs_default.h"
+
+#ifdef MODULE_MTD_SDCARD
+#include "mtd_sdcard.h"
+#include "sdcard_spi.h"
+#include "sdcard_spi_params.h"
+#endif
+
+#if FATFS_FFCONF_OPT_FS_NORTC == 0
+#include "periph/rtc.h"
+#endif
+
+#define FLASH_MOUNT_POINT  "/sd0" /*mount point*/
+
+#define MAIN_QUEUE_SIZE     (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+#define PERIOD              (2U)
+#define REPEAT              (4U)
+
+#define TM_YEAR_OFFSET      (1900)
+#define DELAY_1S   (1U) /* 1 seconds delay between each test */
+
+static io1_xplained_t dev;
+
+int wakeup_gap =200;
+// static unsigned cnt = 0;
+/*RTC struct defination*/
+struct tm coaptime;
+struct tm alarm_time;
+struct tm current_time;
+
+/*RPL*/
+gnrc_ipv6_nib_ft_t entry;      
+void *state = NULL;
+uint8_t dst_address[] = {0};
+// uint8_t nexthop_address[] = {0};
+// int i = 0;
+unsigned iface = 0U;
+
+/*-----------------FAT File System config Start-----------------*/
+static fatfs_desc_t fatfs;
+
+static vfs_mount_t flash_mount = {
+    .mount_point = FLASH_MOUNT_POINT,
+    .fs = &fatfs_file_system,
+    .private_data = (void *)&fatfs,
+};
+#if defined(MODULE_MTD_NATIVE) || defined(MODULE_MTD_MCI)
+/* mtd devices are provided in the board's board_init.c*/
+extern mtd_dev_t *mtd0;
+#endif
+
+#if defined(MODULE_MTD_SDCARD)
+#define SDCARD_SPI_NUM ARRAY_SIZE(sdcard_spi_params)
+
+
+
+extern sdcard_spi_t sdcard_spi_devs[SDCARD_SPI_NUM];
+mtd_sdcard_t mtd_sdcard_devs[SDCARD_SPI_NUM];
+
+/* always default to first sdcard*/
+static mtd_dev_t *mtd_sdcard = (mtd_dev_t*)&mtd_sdcard_devs[0];
+
+#define FLASH_AND_FILESYSTEM_PRESENT    1
+
+#endif
+/*-----------------FAT File System End-----------------------*/
+
+
+static int _cat(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+    /* With newlib or picolibc, low-level syscalls are plugged to RIOT vfs
+     * on native, open/read/write/close/... are plugged to RIOT vfs */
+#if defined(MODULE_NEWLIB) || defined(MODULE_PICOLIBC)
+    FILE *f = fopen(argv[1], "r");
+    if (f == NULL) {
+        printf("file %s does not exist\n", argv[1]);
+        return 1;
+    }
+    char c;
+    while (fread(&c, 1, 1, f) != 0) {
+        putchar(c);
+    }
+    fclose(f);
+#else
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        printf("file %s does not exist\n", argv[1]);
+        return 1;
+    }
+    char c;
+    while (read(fd, &c, 1) != 0) {
+        putchar(c);
+    }
+    close(fd);
+#endif
+    fflush(stdout);
+    return 0;
+}
+
+static int _tee(int argc, char **argv)
+{
+    if (argc != 3) {
+        printf("Usage: %s <file> <str>\n", argv[0]);
+        return 1;
+    }
+
+#if defined(MODULE_NEWLIB) || defined(MODULE_PICOLIBC)
+    FILE *f = fopen(argv[1], "w+");
+    if (f == NULL) {
+        printf("error while trying to create %s\n", argv[1]);
+        return 1;
+    }
+    if (fwrite(argv[2], 1, strlen(argv[2]), f) != strlen(argv[2])) {
+        puts("Error while writing");
+    }
+    fclose(f);
+#else
+    int fd = open(argv[1], O_RDWR | O_CREAT, 00777);
+    if (fd < 0) {
+        printf("error while trying to create %s\n", argv[1]);
+        return 1;
+    }
+    if (write(fd, argv[2], strlen(argv[2])) != (ssize_t)strlen(argv[2])) {
+        puts("Error while writing");
+    }
+    close(fd);
+#endif
+    return 0;
+}
+
+
+static int _mount(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+#if FLASH_AND_FILESYSTEM_PRESENT
+    int res = vfs_mount(&flash_mount);
+    if (res < 0) {
+        printf("Error while mounting %s...try format\n", FLASH_MOUNT_POINT);
+        return 1;
+    }
+
+    printf("%s successfully mounted\n", FLASH_MOUNT_POINT);
+    return 0;
+#else
+    puts("No external flash file system selected");
+    return 1;
+#endif
+}
+
+static int _format(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+#if FLASH_AND_FILESYSTEM_PRESENT
+    int res = vfs_format(&flash_mount);
+    if (res < 0) {
+        printf("Error while formatting %s\n", FLASH_MOUNT_POINT);
+        return 1;
+    }
+
+    printf("%s successfully formatted\n", FLASH_MOUNT_POINT);
+    return 0;
+#else
+    puts("No external flash file system selected");
+    return 1;
+#endif
+}
+
+static int _umount(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+#if FLASH_AND_FILESYSTEM_PRESENT
+    int res = vfs_umount(&flash_mount);
+    if (res < 0) {
+        printf("Error while unmounting %s\n", FLASH_MOUNT_POINT);
+        return 1;
+    }
+
+    printf("%s successfully unmounted\n", FLASH_MOUNT_POINT);
+    return 0;
+#else
+    puts("No external flash file system selected");
+    return 1;
+#endif
+}
+
+
+static const shell_command_t shell_commands[] = {
+    { "cat", "print the content of a file", _cat },
+    { "tee", "write a string in a file", _tee },
+    //{ "mount", "mount flash filesystem", _mount },
+    //{ "format", "format flash file system", _format },
+    //{ "umount", "unmount flash filesystem", _umount },
+    { "coap", "CoAP example", gcoap_cli_cmd },
+    { NULL, NULL, NULL }
+};
+
+
+void print_time(const char *label, const struct tm *time)
+{
+    printf("%s  %04d-%02d-%02d %02d:%02d:%02d\n", label,
+            time->tm_year + TM_YEAR_OFFSET,
+            time->tm_mon + 1,
+            time->tm_mday,
+            time->tm_hour,
+            time->tm_min,
+            time->tm_sec);
+}
+
+static void _sd_card_cid(void)
+{
+    puts("SD Card CID info:");
+    printf("MID: %d\n", dev.sdcard.cid.MID);
+    printf("OID: %c%c\n", dev.sdcard.cid.OID[0], dev.sdcard.cid.OID[1]);
+    printf("PNM: %c%c%c%c%c\n",
+           dev.sdcard.cid.PNM[0], dev.sdcard.cid.PNM[1], dev.sdcard.cid.PNM[2],
+           dev.sdcard.cid.PNM[3], dev.sdcard.cid.PNM[4]);
+    printf("PRV: %u\n", dev.sdcard.cid.PRV);
+    printf("PSN: %" PRIu32 "\n", dev.sdcard.cid.PSN);
+    printf("MDT: %u\n", dev.sdcard.cid.MDT);
+    printf("CRC: %u\n", dev.sdcard.cid.CID_CRC);
+    puts("+----------------------------------------+\n");
+}
+
+
+int main(void)
+{   
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+    server_init();
+
+    puts("gcoap example app");   
+    
+    puts("Waiting for address autoconfiguration...");
+    xtimer_sleep(3);
+
+    /* print network addresses */
+    printf("{\"IPv6 addresses\": [\"");
+    netifs_print_ipv6("\", \"");
+    puts("\"]}");
+    
+    #if MODULE_MTD_SDCARD
+    for(unsigned int i = 0; i < SDCARD_SPI_NUM; i++){
+        mtd_sdcard_devs[i].base.driver = &mtd_sdcard_driver;
+        mtd_sdcard_devs[i].sd_card = &dev.sdcard;
+        mtd_sdcard_devs[i].params = &sdcard_spi_params[i];
+        mtd_init(&mtd_sdcard_devs[i].base);
+    }
+    #endif
+
+    #if defined(MODULE_MTD_NATIVE) || defined(MODULE_MTD_MCI)
+    fatfs.dev = mtd0;
+    #endif
+
+    #if defined(MODULE_MTD_SDCARD)
+    fatfs.dev = mtd_sdcard;
+    #endif
+    
+    puts("gcoap example app");
+    puts("insert SD-card and use 'init' command to set card to spi mode");
+    puts("WARNING: using 'write' or 'copy' commands WILL overwrite data on your sd-card and");
+    puts("almost for sure corrupt existing filesystems, partitions and contained data!");
+
+    float temperature;
+
+    puts("IO1 Xplained extension test application\n");
+    puts("+-------------Initializing------------+\n");
+
+    if (io1_xplained_init(&dev, &io1_xplained_params[0]) != IO1_XPLAINED_OK) {
+        puts("[Error] Cannot initialize the IO1 Xplained extension\n");
+        // return 1;
+    }
+
+    puts("Initialization successful");
+    puts("\n+--------Starting tests --------+");
+    
+    /* Get temperature in degrees celsius */
+    at30tse75x_get_temperature(&dev.temp, &temperature);
+    printf("Temperature [°C]: %i.%03u\n"
+               "+-------------------------------------+\n",
+            (int)temperature,
+            (unsigned)((temperature - (int)temperature) * 1000));
+    ztimer_sleep(ZTIMER_MSEC, DELAY_1S);
+
+    /* Card detect pin is inverted */
+    if (!gpio_read(IO1_SDCARD_SPI_PARAM_DETECT)) {
+        _sd_card_cid();
+        ztimer_sleep(ZTIMER_MSEC, DELAY_1S);
+    }
+
+    uint16_t light;
+    io1_xplained_read_light_level(&light);
+    printf("Light level: %i\n"
+            "+-------------------------------------+\n",
+            light);
+    ztimer_sleep(ZTIMER_MSEC, DELAY_1S);
+
+    /* set led */
+    gpio_set(IO1_LED_PIN);
+    ztimer_sleep(ZTIMER_MSEC, DELAY_1S);
+
+    /* clear led */
+    gpio_clear(IO1_LED_PIN);
+    ztimer_sleep(ZTIMER_MSEC, DELAY_1S);
+
+    /* toggle led */
+    gpio_toggle(IO1_LED_PIN);
+    ztimer_sleep(ZTIMER_MSEC, DELAY_1S);
+
+    /* toggle led again */
+    gpio_toggle(IO1_LED_PIN);
+    ztimer_sleep(ZTIMER_MSEC, DELAY_1S);
+
+
+    /*11111111111111111*/
+    vfs_format(&flash_mount);
+
+    vfs_DIR mount = {0};
+    
+
+    /* list mounted file systems */
+
+    puts("mount points:");
+    while (vfs_iterate_mount_dirs(&mount)) {
+        printf("\t%s\n", mount.mp->mount_point);
+    }
+    
+    //printf("\ndata dir: %s\n", VFS_DEFAULT_DATA);
+    
+    /*------------Typical file create and write test start--------------*/
+
+    /*11111111111111111*/
+    vfs_mount(&flash_mount);
+
+    char data_file_path[] = "/sd0/DATA.TXT";
+    int fo = open(data_file_path, O_RDWR | O_CREAT, 00777);
+    if (fo < 0) {
+        printf("error while trying to create %s\n", data_file_path);
+        return 1;
+    }
+    else{
+        puts("creating file success");
+    }
+    char test_data[] = "testtesttesttesttest";
+
+
+    if (write(fo, test_data, strlen(test_data)) != (ssize_t)strlen(test_data)) {
+        puts("Error while writing");
+    }
+    close(fo);
+    int fr = open(data_file_path, O_RDONLY | O_CREAT, 00777);  //before open with O_RDWR which 
+                                                               //will conflict with open(file)
+                                                               //open(file)will equal 0, have to beb a O_RDPNLY for read
+    // char data_buf[sizeof(test_data)];
+    // printf("data:[],length=");
+    // vfs_read(fo,data_buf,sizeof(test_data));    
+    // printf("data:[],length=");
+    char c;
+
+    while (read(fr, &c, 1) != 0){
+    putchar(c);  //printf won't work here
+    }
+    puts("\n");
+       
+    close(fo);
+    puts("closing file");
+
+    /*11111111111111111*/
+    vfs_umount(&flash_mount);
+    puts("flash point umount");
+
+    /*------------Typical file create and write test end----------------*/
+
+    /*-------------CoAP CLient with RTC config and init Start------------*/
+/* for the thread running the shell */
+    struct tm time = {
+        .tm_year = 2020 - TM_YEAR_OFFSET,   /* years are counted from 1900 */
+        .tm_mon  =  4,                      /* 0 = January, 11 = December */
+        .tm_mday = 28,
+        .tm_hour = 23,
+        .tm_min  = 58,
+        .tm_sec  = 57
+    };   
+    rtc_init();
+    puts("1111111111111111111111111111111111");
+    print_time("  Setting clock to ", &time);
+    rtc_set_time(&time);
+    rtc_get_time(&time);	
+    
+    print_time("Clock value is now ", &time);
+    
+    
+
+
+    puts("Configured rpl:");
+    gnrc_rpl_init(7);
+    puts("printing route:");
+    while (gnrc_ipv6_nib_ft_iter(NULL, iface, &state, &entry)) {
+        char addr_str[IPV6_ADDR_MAX_STR_LEN];
+        if ((entry.dst_len == 0) || ipv6_addr_is_unspecified(&entry.dst)) {
+            printf("default%s ", (entry.primary ? "*" : ""));
+             puts("printing route:");   
+
+        }
+        else {
+            printf("%s/%u ", ipv6_addr_to_str(addr_str, &entry.dst, sizeof(addr_str)),entry.dst_len);
+            puts("printing route:");
+        }
+        if (!ipv6_addr_is_unspecified(&entry.next_hop)) {
+           printf("via %s ", ipv6_addr_to_str(addr_str, &entry.next_hop, sizeof(addr_str)));
+           puts("printing route:");
+        }
+        // printf("dev #%u\n", fte->iface);
+        // char a[i][4] = entry->iface;
+       // i++;
+    }
+
+    
+
+    /* start shell */
+    puts("All up, running the shell now");
+
+    puts("RIOT network stack example application");
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
---
+++ examples/fatfsgcoap/server.c
@@ -0,0 +1,389 @@
+/*
+ * Copyright (c) 2015-2017 Ken Bannister. All rights reserved.
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       gcoap CLI support
+ *
+ * @author      Ken Bannister <kb2ma@runbox.com>
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fmt.h"
+#include "net/gcoap.h"
+#include "net/utils.h"
+#include "od.h"
+
+#include "gcoap_example.h"
+
+#define ENABLE_DEBUG 0
+#include "debug.h"
+
+/*IPv6 and RPL*/
+#include "net/ipv6/addr.h"
+#include "net/gnrc/ipv6/nib.h"
+#include "net/gnrc/ipv6/nib/ft.h"
+#include "net/gnrc/netif.h"
+#include "net/ipv6/addr.h"
+
+/*RTC*/
+#include "periph_conf.h"
+#include "periph/rtc.h"
+// #include "periph/rtc_mem.h"
+
+/*Networking API - netapi/netopt/netdev*/
+#include "net/gnrc/netif.h"
+#include "net/netif.h"
+#include "net/gnrc/netapi.h"
+
+#if IS_USED(MODULE_GCOAP_DTLS)
+#include "net/credman.h"
+#include "net/dsm.h"
+#include "tinydtls_keys.h"
+
+/* Example credential tag for credman. Tag together with the credential type needs to be unique. */
+#define GCOAP_DTLS_CREDENTIAL_TAG 10
+
+static const uint8_t psk_id_0[] = PSK_DEFAULT_IDENTITY;
+static const uint8_t psk_key_0[] = PSK_DEFAULT_KEY;
+static const credman_credential_t credential = {
+    .type = CREDMAN_TYPE_PSK,
+    .tag = GCOAP_DTLS_CREDENTIAL_TAG,
+    .params = {
+        .psk = {
+            .key = { .s = psk_key_0, .len = sizeof(psk_key_0) - 1, },
+            .id = { .s = psk_id_0, .len = sizeof(psk_id_0) - 1, },
+        }
+    },
+};
+#endif
+
+static ssize_t _encode_link(const coap_resource_t *resource, char *buf,
+                            size_t maxlen, coap_link_encoder_ctx_t *context);
+static ssize_t _stats_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx);
+static ssize_t _riot_board_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx);
+static ssize_t _timetest_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx);
+static ssize_t _rpl_table_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx);
+static ssize_t _systime_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx);
+
+
+/* CoAP resources. Must be sorted by path (ASCII order). */
+static const coap_resource_t _resources[] = {
+    { "/cli/stats", COAP_GET | COAP_PUT, _stats_handler, NULL },
+    { "/riot/board", COAP_GET, _riot_board_handler, NULL },
+    { "/riot/r", COAP_GET, _rpl_table_handler, NULL },
+    { "/riot/time", COAP_GET, _timetest_handler, NULL },
+    { "/riot/systime", COAP_GET | COAP_PUT, _systime_handler, NULL },
+};
+
+static const char *_link_params[] = {
+    ";ct=0;rt=\"count\";obs",
+    NULL
+};
+
+static gcoap_listener_t _listener = {
+    &_resources[0],
+    ARRAY_SIZE(_resources),
+    GCOAP_SOCKET_TYPE_UNDEF,
+    _encode_link,
+    NULL,
+    NULL
+};
+
+
+/* Adds link format params to resource list */
+static ssize_t _encode_link(const coap_resource_t *resource, char *buf,
+                            size_t maxlen, coap_link_encoder_ctx_t *context) {
+    ssize_t res = gcoap_encode_link(resource, buf, maxlen, context);
+    if (res > 0) {
+        if (_link_params[context->link_pos]
+                && (strlen(_link_params[context->link_pos]) < (maxlen - res))) {
+            if (buf) {
+                memcpy(buf+res, _link_params[context->link_pos],
+                       strlen(_link_params[context->link_pos]));
+            }
+            return res + strlen(_link_params[context->link_pos]);
+        }
+    }
+
+    return res;
+}
+
+/* _systime_handler time structure defination*/
+extern void print_time(const char *label, const struct tm *time);
+struct tm handler_time;
+
+static ssize_t _systime_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx)
+{
+    (void)ctx;
+
+    /* read coap method type in packet */
+    unsigned method_flag = coap_method2flag(coap_get_code_detail(pdu));
+
+    switch (method_flag) {
+        case COAP_GET:
+            rtc_get_time(&handler_time);
+            int payload_time = mktime(&handler_time);
+            gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+            coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
+            size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
+
+            /* write the response buffer with the request count value */
+            resp_len += fmt_u32_dec((char *)pdu->payload, payload_time);
+            return resp_len;
+
+        case COAP_PUT:
+            /* convert the payload to an integer and update the internal
+               value */
+            if (pdu->payload_len <= 10) {
+                puts("<=11\n");
+                char payload[11] = { 0 };
+                puts("received\n");
+                memcpy(payload, (char *)pdu->payload, pdu->payload_len);
+                puts("memery allocation\n");
+                printf("%s",payload);
+                req_count = strtoull(payload, NULL, 10);
+                printf("%d\n", req_count);
+                puts("have the number\n");
+                rtc_localtime((int)req_count, &handler_time);
+                rtc_set_time(&handler_time);
+                print_time("This is the current system time",&handler_time);
+                return gcoap_response(pdu, buf, len, COAP_CODE_CHANGED);
+            }
+            else {
+                return gcoap_response(pdu, buf, len, COAP_CODE_BAD_REQUEST);
+            }
+    }
+
+    return 0;
+}
+
+
+static ssize_t _rpl_table_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx)
+{
+    (void)ctx;
+    gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+    coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
+    size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
+
+    /**************************/
+    //for rpl table reply//
+    int num_node = 10;
+    gnrc_ipv6_nib_ft_t entry;      
+    void *state = NULL;
+    unsigned iface = 0U;
+    char rsp[num_node*30];
+    memset(rsp, 0, sizeof(rsp));
+    /*************************/
+    char dst_char[4] = "dst/";
+    char next_char[4] = "via/";
+    // char newline[1] = {'\n'};
+    // ssize_t p = 0;
+    //char rsp[16];
+    // unsigned code = COAP_CODE_EMPTY;
+
+    /**************************/
+    //for rpl table reply//
+     while (gnrc_ipv6_nib_ft_iter(NULL, iface, &state, &entry)) {
+        char addr_str[IPV6_ADDR_MAX_STR_LEN];
+        if ((entry.dst_len == 0) || ipv6_addr_is_unspecified(&entry.dst)) {
+        }
+        else {
+            ipv6_addr_to_str(addr_str, &entry.dst, sizeof(addr_str));
+            puts("------------\n");   
+            printf("%d", sizeof(addr_str));
+            puts("------------\n");   
+            printf("%s", addr_str);
+            // puts("-------------\n");   
+            //uint8_t dst_address[20] ;
+            //fmt_hex_bytes(dst_address, addr_str);
+            strcat(rsp, dst_char);
+            for (int i=0;i<11; i++){
+                rsp[i+4]=addr_str[i];
+            }
+            strcat(rsp, "\n");
+        }
+        if (!ipv6_addr_is_unspecified(&entry.next_hop)) {
+            ipv6_addr_to_str(addr_str, &entry.next_hop, sizeof(addr_str));
+            strcat(rsp,next_char);
+            strcat(rsp, addr_str);
+            strcat(rsp, "\n");
+        }
+    }
+
+    /* write the RIOT board name in the response buffer */
+    if (pdu->payload_len >= strlen(rsp)) {
+        memcpy(pdu->payload, rsp, strlen(rsp));
+        return resp_len + strlen(rsp);
+    }
+    else {
+        puts("gcoap_cli: msg buffer too small");
+        return gcoap_response(pdu, buf, len, COAP_CODE_INTERNAL_SERVER_ERROR);
+    }
+    
+
+    /**************************/
+
+    // resp_len += fmt_str((char *)pdu->payload, rsp);
+    // return resp_len;  
+
+}
+
+static ssize_t _timetest_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx)
+{
+    (void)ctx;
+
+    /* read coap method type in packet */
+    unsigned method_flag = coap_method2flag(coap_get_code_detail(pdu));
+
+    switch (method_flag) {
+        case COAP_GET:
+            gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+            coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
+            size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
+
+            /* write the response buffer with the request count value */
+            resp_len += fmt_u16_dec((char *)pdu->payload, req_count);
+            return resp_len;
+
+        case COAP_PUT:
+            /* convert the payload to an integer and update the internal
+               value */
+            if (pdu->payload_len <= 12) {
+                puts("<=11\n");
+                char payload[13] = { 0 };
+                puts("received\n");
+                memcpy(payload, (char *)pdu->payload, pdu->payload_len);
+                puts("memery allocation\n");
+                printf("%s",payload);
+                req_count = strtoull(payload, NULL, 10);
+                printf("%d\n", req_count);
+                puts("have the number\n");
+                return gcoap_response(pdu, buf, len, COAP_CODE_CHANGED);
+            }
+            else {
+                return gcoap_response(pdu, buf, len, COAP_CODE_BAD_REQUEST);
+            }
+    }
+
+    return 0;
+}
+
+/*
+ * Server callback for /cli/stats. Accepts either a GET or a PUT.
+ *
+ * GET: Returns the count of packets sent by the CLI.
+ * PUT: Updates the count of packets. Rejects an obviously bad request, but
+ *      allows any two byte value for example purposes. Semantically, the only
+ *      valid action is to set the value to 0.
+ */
+static ssize_t _stats_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx)
+{
+    (void)ctx;
+
+    /* read coap method type in packet */
+    unsigned method_flag = coap_method2flag(coap_get_code_detail(pdu));
+
+    switch (method_flag) {
+        case COAP_GET:
+            gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+            coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
+            size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
+
+            /* write the response buffer with the request count value */
+            resp_len += fmt_u16_dec((char *)pdu->payload, req_count);
+            return resp_len;
+
+        case COAP_PUT:
+            /* convert the payload to an integer and update the internal
+               value */
+            if (pdu->payload_len <= 5) {
+                char payload[6] = { 0 };
+                memcpy(payload, (char *)pdu->payload, pdu->payload_len);
+                req_count = (uint16_t)strtoul(payload, NULL, 10);
+                return gcoap_response(pdu, buf, len, COAP_CODE_CHANGED);
+            }
+            else {
+                return gcoap_response(pdu, buf, len, COAP_CODE_BAD_REQUEST);
+            }
+    }
+
+    return 0;
+}
+
+static ssize_t _riot_board_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx)
+{
+    (void)ctx;
+    gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+    coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
+    size_t resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
+
+    /* write the RIOT board name in the response buffer */
+    if (pdu->payload_len >= strlen(RIOT_BOARD)) {
+        memcpy(pdu->payload, RIOT_BOARD, strlen(RIOT_BOARD));
+        return resp_len + strlen(RIOT_BOARD);
+    }
+    else {
+        puts("gcoap_cli: msg buffer too small");
+        return gcoap_response(pdu, buf, len, COAP_CODE_INTERNAL_SERVER_ERROR);
+    }
+}
+
+void notify_observers(void)
+{
+    size_t len;
+    uint8_t buf[CONFIG_GCOAP_PDU_BUF_SIZE];
+    coap_pkt_t pdu;
+
+    /* send Observe notification for /cli/stats */
+    switch (gcoap_obs_init(&pdu, &buf[0], CONFIG_GCOAP_PDU_BUF_SIZE,
+            &_resources[0])) {
+    case GCOAP_OBS_INIT_OK:
+        DEBUG("gcoap_cli: creating /cli/stats notification\n");
+        coap_opt_add_format(&pdu, COAP_FORMAT_TEXT);
+        len = coap_opt_finish(&pdu, COAP_OPT_FINISH_PAYLOAD);
+        len += fmt_u16_dec((char *)pdu.payload, req_count);
+        gcoap_obs_send(&buf[0], len, &_resources[0]);
+        break;
+    case GCOAP_OBS_INIT_UNUSED:
+        DEBUG("gcoap_cli: no observer for /cli/stats\n");
+        break;
+    case GCOAP_OBS_INIT_ERR:
+        DEBUG("gcoap_cli: error initializing /cli/stats notification\n");
+        break;
+    }
+}
+
+void server_init(void)
+{
+#if IS_USED(MODULE_GCOAP_DTLS)
+    int res = credman_add(&credential);
+    if (res < 0 && res != CREDMAN_EXIST) {
+        /* ignore duplicate credentials */
+        printf("gcoap: cannot add credential to system: %d\n", res);
+        return;
+    }
+    sock_dtls_t *gcoap_sock_dtls = gcoap_get_sock_dtls();
+    res = sock_dtls_add_credential(gcoap_sock_dtls, GCOAP_DTLS_CREDENTIAL_TAG);
+    if (res < 0) {
+        printf("gcoap: cannot add credential to DTLS sock: %d\n", res);
+    }
+#endif
+
+    gcoap_register_listener(&_listener);
+}
